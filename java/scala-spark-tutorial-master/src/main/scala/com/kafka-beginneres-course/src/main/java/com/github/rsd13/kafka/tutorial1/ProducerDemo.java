41c772938b0ddc4e916e71937cb7965a8fe9b6f9
rsd13 <rsoriadiez@gmail.com>
2022-01-03 15:47:22 +0100

    escrito clase ProducerDemoKeys

--- java/scala-spark-tutorial-master/src/main/scala/com/kafka-beginneres-course/src/main/java/com/github/rsd13/kafka/tutorial1/ProducerDemo.java
+++ java/scala-spark-tutorial-master/src/main/scala/com/kafka-beginneres-course/src/main/java/com/github/rsd13/kafka/tutorial1/ProducerDemo.java
@@ -1,9 +1,38 @@
 package com.github.rsd13.kafka.tutorial1;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
 public class ProducerDemo {
 
     public static void main(String[] args) {
-        System.out.println("hello word");
+        String bootstrapServers = "localhost:9092";
+
+        //propiedades del productor
+        Properties properties = new Properties();
+        // añadimos las propiedades de https://kafka.apache.org/documentation/#configuration
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServers);
+        //key y value ayuda a saber que tipo de valor esta enviando a Kafka para serilizarlo
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+
+        //constructor del productor con key/value a String
+        KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
+
+        //crear productor record
+        ProducerRecord<String,String > record =
+                new ProducerRecord<String, String>("first_topic","hola mundo!");
+
+        //enviar datos
+        producer.send(record);
+        //Espere a que se entreguen todos los mensajes de la cola del productor.
+        producer.flush();
+        producer.close();
 
     }
 }
--- java/scala-spark-tutorial-master/src/main/scala/com/kafka-beginneres-course/src/main/java/com/github/rsd13/kafka/tutorial1/ProducerDemoKeys.java
+++ java/scala-spark-tutorial-master/src/main/scala/com/kafka-beginneres-course/src/main/java/com/github/rsd13/kafka/tutorial1/ProducerDemoKeys.java
@@ -6,11 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
-public class ProducerDemoWithCallback {
+public class ProducerDemoKeys {
 
-    public static void main(String[] args) {
-        Logger logger =LoggerFactory.getLogger((ProducerDemoWithCallback.class));
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Logger logger =LoggerFactory.getLogger((ProducerDemoKeys.class));
         String bootstrapServers = "localhost:9092";
 
         //propiedades del productor
@@ -25,9 +26,29 @@ public class ProducerDemoWithCallback {
         //constructor del productor con key/value a String
         KafkaProducer<String,String> producer = new KafkaProducer<String, String>(properties);
         for(int i = 0; i<10;i++) {
+            String topic = "first_topic";
+            String value = "hola mundo" + i;
+            String key = "Key_" + i;
+
             //crear productor record
             ProducerRecord<String, String> record =
-                    new ProducerRecord<String, String>("first_topic", "hola mundo " + i);
+                    new ProducerRecord<String, String>(topic, key, value);
+            logger.info("Key: " + key);
+            /*
+            A poner una key se garantiza que esa key siempre vaya a la misma partición
+            id_0-> partition: 1
+            id_1-> partition: 2
+            id_2-> partition: 2
+            id_3-> partition: 0
+            id_4-> partition: 2
+            id_5-> partition: 2
+            id_6-> partition: 1
+            id_7-> partition: 1
+            id_8-> partition: 0
+            id_9-> partition: 0
+             */
+
+
 
             //enviar datos
             producer.send(record, new Callback() {
@@ -47,7 +68,7 @@ public class ProducerDemoWithCallback {
 
                     }
                 }
-            });
+            }).get(); //bloquea el envio para hacerlo sincrono (NO HACERLO EN PRODUCCIÓN)
         }
         //Espere a que se entreguen todos los mensajes de la cola del productor.
         producer.flush();
